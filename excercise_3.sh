115c402760da9bd52412b06f5120dbb92e1ef5e2
giuls99 <104635363+giuls99@users.noreply.github.com>
2023-03-16 00:49:18 +0100

    Update excercise_3.sh

--- excercise_3.sh
+++ excercise_3.sh
@@ -1,1 +1,1 @@
-0 20 * * 0 tar -czf /home/user/backups/$(date +\%Y\%m\%d)_backup.tar.gz /home/user && scp /home/user/backups/*.tar.gz user@192.168.1.100:/backup/folder
+0 21 * * 0 tar -czf /home/user/backups/$(date +\%Y\%m\%d)_backup.tar.gz /home/user && scp /home/user/backups/*.tar.gz user@192.168.1.100:/backup/folder
